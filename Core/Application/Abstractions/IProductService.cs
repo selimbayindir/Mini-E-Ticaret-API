3ee048c9ee2c6293ac7b19e90d1ee07174c9400e
byndr <byndr28@gmail.com>
2023-01-04 00:17:09 +0300

    Add project files.

---
+++ Core/Application/Abstractions/IProductService.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Abstractions
+{
+    public interface IProductService
+    {
+        string GetProductName(string productName);
+        List<Product> GetProducts();
+
+    }
+}
---
+++ Presentation/EtradeAPI/Controllers/InMemoryProductsController.cs
@@ -0,0 +1,25 @@
+﻿using Application.Abstractions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EtradeAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InMemoryProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public InMemoryProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet("inmemorygetallproducts")]
+        public IActionResult GetAllProducts()
+        {
+            var products =_productService.GetProducts();
+            return Ok(products);
+        }
+    }
+}
